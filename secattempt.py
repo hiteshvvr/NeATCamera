4d921bb9e70731b5d4d6b90b3e9e79fce4884da6
Hitesh <hitesh.vvr@gmail.com>
2019-03-17 05:07:21 +0200

    sth

--- secattempt.py
+++ secattempt.py
@@ -1,63 +1,18 @@
 import sys
 from PyQt4 import QtGui, QtCore
 import cv2
+import time
 
-class QtCapture(QtGui.QWidget):
-    def __init__(self, *args):
-        super(QtGui.QWidget, self).__init__()
 
-        self.fps = 24
-        self.cap = cv2.VideoCapture(*args)
+class MCamera:
+    def __init__(self):
+        self.cam = cv2.VideoCapture(0)
 
-        self.video_frame = QtGui.QLabel()
-        lay = QtGui.QVBoxLayout()
-        lay.setMargin(0)
-        lay.addWidget(self.video_frame)
-        self.setLayout(lay)
+    def getimg(self):
+        self.rval, self.img = self.cam.read()
+        return(self.img)
+                # time.sleep(0.1)
 
-        # ------ Modification ------ #
-        self.isCapturing = False
-        self.ith_frame = 1
-        # ------ Modification ------ #
-
-    def setFPS(self, fps):
-        self.fps = fps
-
-    def nextFrameSlot(self):
-        ret, frame = self.cap.read()
-
-        # ------ Modification ------ #
-        # Save images if isCapturing
-        if self.isCapturing:
-            cv2.imwrite('img_%05d.jpg'%self.ith_frame, frame)
-            self.ith_frame += 1
-        # ------ Modification ------ #
-
-        # My webcam yields frames in BGR format
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        img = QtGui.QImage(frame, frame.shape[1], frame.shape[0], QtGui.QImage.Format_RGB888)
-        pix = QtGui.QPixmap.fromImage(img)
-        self.video_frame.setPixmap(pix)
-
-    def start(self):
-        self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.nextFrameSlot)
-        self.timer.start(1000./self.fps)
-
-    def stop(self):
-        self.timer.stop()
-
-    # ------ Modification ------ #
-    def capture(self):
-        if not self.isCapturing:
-            self.isCapturing = True
-        else:
-            self.isCapturing = False
-    # ------ Modification ------ #
-
-    def deleteLater(self):
-        self.cap.release()
-        super(QtGui.QWidget, self).deleteLater()
 
 
 class ControlWindow(QtGui.QWidget):
@@ -65,14 +20,12 @@ class ControlWindow(QtGui.QWidget):
         QtGui.QWidget.__init__(self)
         self.capture = None
 
-        self.start_button = QtGui.QPushButton('Start')
+        self.start_button = QtGui.QPushButton('Start Camera')
         self.start_button.clicked.connect(self.startCapture)
-        self.quit_button = QtGui.QPushButton('End')
+        self.quit_button = QtGui.QPushButton('End Camera')
         self.quit_button.clicked.connect(self.endCapture)
         self.end_button = QtGui.QPushButton('Stop')
 
-        self.getroi_button = QtGui.QPushButton('Get ROI')
-        self.getroi_button.clicked.connect(self.getROI)
         # ------ Modification ------ #
         self.capture_button = QtGui.QPushButton('Capture')
         self.capture_button.clicked.connect(self.saveCapture)
@@ -82,7 +35,6 @@ class ControlWindow(QtGui.QWidget):
         vbox.addWidget(self.start_button)
         vbox.addWidget(self.end_button)
         vbox.addWidget(self.quit_button)
-        vbox.addWidget(self.getroi_button)
 
         # ------ Modification ------ #
         vbox.addWidget(self.capture_button)
@@ -94,29 +46,19 @@ class ControlWindow(QtGui.QWidget):
         self.show()
 
     def startCapture(self):
-        if not self.capture:
-            self.capture = QtCapture(0)
-            self.end_button.clicked.connect(self.capture.stop)
-            # self.capture.setFPS(1)
-            self.capture.setParent(self)
-            self.capture.setWindowFlags(QtCore.Qt.Tool)
-        self.capture.start()
-        self.capture.show()
-
-
-    def getROI(self):
-        self.rx1 = 0 
-        self.rx2 = 0 
-        self.ry1 = 0 
-        self.ry2 = 0 
-        if not self.capture:
-            self.capture = QtCapture(0)
-
-        self.retval, self.timage = self.capture.cap.read()
-        self.r = cv2.selectROI("Image", im, fromCenter, showCrosshair)
-
-
-
+        self.mcamera = MCamera()
+        # print(self.img)
+        while True:
+            self.img = self.mcamera.getimg()
+            cv2.imshow('Live Image', self.img)
+        # if not self.capture:
+        #     self.capture = QtCapture(0)
+        #     self.end_button.clicked.connect(self.capture.stop)
+        #     # self.capture.setFPS(1)
+        #     self.capture.setParent(self)
+        #     self.capture.setWindowFlags(QtCore.Qt.Tool)
+        # self.capture.start()
+        # self.capture.show()
 
     def endCapture(self):
         self.capture.deleteLater()
